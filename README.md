24f92a533fbb29ccc7b1d2297ad3cb12fa475e14
trick <trick.developer@gmail.com>
2015-05-11 08:13:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+## pwdshort ##
+`pwdshort` is a utility for linux written in Go used to shorten the $PWD for display in the terminal, such as in a bash PS1 prompt.
+
+---
+### Compiling ###
+---
+
+To compile, clone the repository and run `$ go build pwdshort`.
+
+---
+### Bash Usage ###
+---
+
+Open ~/.bashrc in an editor, and update your PS1 variable definition:
+```bash
+    pathAbbrev='/path/to/pwdshort-binary'
+    PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\n\e[01;36;19m`${pathAbbrev}`\e[0m\$ '
+```
+
+---
+### Sample Output ###
+---
+
+Assuming the current directory is:
+`/home/user/Development/projects/go/src/pwdshort`;
+the path displayed in bash would be:
+`~/Dev/.../src/pwdshort`
+
+Paths with up to four parts are displayed as-is.
+
+---
+### License ###
+---
+`pwdshort` is available for free under the <a href="LICENSE">MIT License</a>.
---
+++ pwdshort.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+  //number of path parts to display at the front of the output
+	var maxDisplayedPartsPrefix = 2
+  //number of path parts to display at the end of the output	
+	var maxDisplayedPartsSuffix = 2
+	
+	dst := string(os.Getenv("PWD"))
+
+	var sa []string
+	sa = strings.Split(dst, "/")
+
+	var bSkipNext = false
+	var bInHomeDir = false
+	var totalParts = len(sa)
+	var displayedParts = 0
+	var displayedElipsis = false
+	var newPathStrs []string
+
+	for i, value := range sa {
+		var replacement string
+		if bSkipNext {
+			bSkipNext = false
+			continue
+		}
+		
+		if len(value) > 0 {
+		  //custom path-part replacements
+			switch value {
+			case "home":
+				//make sure it's the /home directory
+				if i == 1 {
+					replacement = "~"
+					bInHomeDir = true
+					bSkipNext = true
+				} 
+			case "Development":
+				replacement = "Dev"
+			case "projects":
+				replacement = "proj"
+			default:
+				replacement = value
+			}
+			value := replacement
+			
+			displayedParts++
+			
+			if displayedParts > maxDisplayedPartsPrefix && i < totalParts-maxDisplayedPartsSuffix {
+				if !displayedElipsis {
+					newPathStrs = append(newPathStrs, "...")
+					displayedElipsis = true
+				}
+			} else {
+		      newPathStrs = append(newPathStrs, value)
+			}
+		}
+	}
+
+	sPrefixSlash := "/"
+	if bInHomeDir {
+	  sPrefixSlash = ""
+	}
+	
+	fmt.Printf("%s%s", sPrefixSlash, strings.TrimSpace(strings.Join(newPathStrs, "/")))
+}
